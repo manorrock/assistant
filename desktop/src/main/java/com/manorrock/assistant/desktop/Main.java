c06e6e86415c3bc2b5a8717f7ae7dcbe0398f7d4
Manfred Riem <mriem@manorrock.com>
2025-03-13 18:08:55 -0500

    Fixes #40 - Refactor desktop application to correct package naming (#41)

--- desktop/src/main/java/com/manorrock/assistant/desktop/Main.java
+++ desktop/src/main/java/com/manorrock/assistant/desktop/Main.java
@@ -1,10 +1,8 @@
 package com.manorrock.assistant.desktop;
 
-import com.example.App;
-
 public class Main {
 
     public static void main(String[] args) {
-        App.main(args);
+        DesktopApplication.main(args);
     }
 }
