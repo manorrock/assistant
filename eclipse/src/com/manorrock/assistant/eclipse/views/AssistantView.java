11f2d789cc9af19fa366bb3abebd349e5abdbdd6
Manfred Riem <mriem@manorrock.com>
2025-03-23 08:18:12 -0500

    Fixes #166 - Add building Eclipse update site to build workflow

--- eclipse/src/com/manorrock/assistant/eclipse/views/AssistantView.java
+++ eclipse/src/com/manorrock/assistant/eclipse/views/AssistantView.java
@@ -45,12 +45,8 @@ import org.json.JSONObject;
 
 import com.manorrock.assistant.shared.Command;
 import com.manorrock.assistant.shared.CommandRegistry;
-<<<<<<< Updated upstream
-import com.manorrock.assistant.shared.LlmConfiguration;
-=======
 import com.manorrock.assistant.shared.DeprecatedCommand;
-import com.manorrock.assistant.llm.LlmConfiguration;
->>>>>>> Stashed changes
+import com.manorrock.assistant.shared.LlmConfiguration;
 import com.manorrock.assistant.shared.LlmModelCommand;
 import com.manorrock.assistant.shared.NewCommand;
 import com.manorrock.assistant.shared.SourceCommand;
