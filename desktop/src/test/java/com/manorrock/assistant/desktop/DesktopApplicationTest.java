426c6dc44c8a29b63b4cbb4a771ffe9b0c4ebbf9
Manfred Riem <mriem@manorrock.com>
2025-04-05 18:01:21 -0500

    Fixes #301 - Add application launch test for Desktop application (#302)

---
+++ desktop/src/test/java/com/manorrock/assistant/desktop/DesktopApplicationTest.java
@@ -0,0 +1,25 @@
+package com.manorrock.assistant.desktop;
+
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
+import org.testfx.framework.junit5.ApplicationTest;
+
+public class DesktopApplicationTest extends ApplicationTest {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        new DesktopApplication().start(stage);
+    }
+
+    @Test
+    @DisabledIfEnvironmentVariable(named = "GITHUB_ACTIONS", matches = "true")
+    public void testApplicationLaunches() {
+        assertThat(lookup("#root").queryAs(BorderPane.class), is(notNullValue()));
+    }
+}
